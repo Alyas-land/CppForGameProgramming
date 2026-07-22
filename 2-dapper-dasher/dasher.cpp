c3f4bb8267fcf812cac0bbd337ce391dbee7218f
a.mashayekhi90@gmail.com <a.mashayekhi90@gmail.com>
2026-07-22 19:49:57 +0330

    Add gavity and velocity system for platformer mechanic

--- 2-dapper-dasher/dasher.cpp
+++ 2-dapper-dasher/dasher.cpp
@@ -11,15 +11,49 @@
 int main(){
 
     // Set screen game size
-    int ScreenWidth{800};
-    int ScreenHeight{600};
+    const int ScreenWidth{800};
+    const int ScreenHeight{600};
+
+    // Acceleration due to gravity
+    float gravity{1.0f};
+    float jumpVel{15.0f};
+    bool isGrounded{false};
+
+
+    // Rectangle dimensions
+    Vector2 rectangleSize{50.0f, 80.0f};
+
+    float posY{ScreenHeight - rectangleSize.y};
+    float velocity{0.0f};
+
     InitWindow(ScreenWidth, ScreenHeight, "Dapper Dasher");
     SetTargetFPS(60);
 
     while(!WindowShouldClose()){
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
+    
+        // Check object on the ground
+        if (posY >= (ScreenHeight - rectangleSize.y)){
+            //rectanglr on the ground
+            velocity = 0.0f;
+            isGrounded = true;
+        }
+        else {
+            // Rectangle on the air
+            velocity += gravity;
+        }
+        // Check if space key is pressed and rectangle is on the ground
+        if(IsKeyDown(KEY_SPACE) && isGrounded){
+            velocity -= 30.0f;
+            isGrounded = false;
+        }
+        // Update position
+        posY += velocity;
 
+
+        DrawRectangleV({ScreenWidth/2, posY}, rectangleSize, BLUE);
 
 
 
